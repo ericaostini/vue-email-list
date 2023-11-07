7215b37a2e4b9b3d9eee651bd48c2badcb852897
ericaostini <ericaostini14@gmail.com>
2023-11-07 15:10:13 +0100

    display 10 emails with for loop

--- js/script.js
+++ js/script.js
@@ -10,9 +10,11 @@ createApp({
         
     },
     mounted() {
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
+        for (let i = 0 ; i < 10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
             console.log(resp.data.response)
             this.emails.push(resp.data.response)
-        })
+            })
+        }
     },
 }).mount("#app")
