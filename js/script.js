b3844cf77e28d3a76cfe1d2ff454393c44cd015f
ericaostini <ericaostini14@gmail.com>
2023-11-07 14:49:08 +0100

    display one email in document

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-            
+            emails: []
         }
     },
     methods: {
@@ -12,6 +12,7 @@ createApp({
     mounted() {
         axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
             console.log(resp.data.response)
+            this.emails.push(resp.data.response)
         })
     },
 }).mount("#app")
