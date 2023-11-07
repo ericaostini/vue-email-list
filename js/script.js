29e17be9d0aeef7c11e215c2ebb0f9159547da1b
ericaostini <ericaostini14@gmail.com>
2023-11-07 14:47:19 +0100

    axios get API and display it in console

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ createApp({
         
     },
     mounted() {
-        
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
+            console.log(resp.data.response)
+        })
     },
 }).mount("#app")
